8caf7f56a7d72b53627df45a72a359e55b8b0067
Laura Valdivia <laura.marcq@protonmail.com>
2024-06-24 15:27:32 +0100

    add migrations model and seeder for games table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         $this->call(GameListSeeder::class);
         $this->call(ListItemSeeder::class);
+        $this->call(GameSeeder::class);
     }
 
 

183c5b54f2efca2e365e972b0d188b1a9b0ca615
Laura Valdivia <laura.marcq@protonmail.com>
2024-07-10 10:18:15 +0100

    fix icon not rendering correctly

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link rel="icon" href="logo.ico" type="image/x-icon">
+    <link rel="icon" href="/logo.ico" type="image/x-icon">
 
     <!-- Scripts -->
     <!-- <script src="https://unpkg.com/vue-multiselect"></script> -->
